87fb3cdf9328880b31469af19b242f51d75f8d18
Wilfred Litwachi <katuka.wilfred8@gmail.com>
2018-02-26 12:29:43 +0300

    hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
